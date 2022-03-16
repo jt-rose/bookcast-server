eddac4608636868ad848d6314748c2a761155c91
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-15 21:23:49 -0400

    set up drafts of sql tables

--- cast_api/models.py
+++ cast_api/models.py
@@ -6,3 +6,63 @@ class User(models.Model):
     password = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     
+# class BookCast(models.Model):
+#     user_id = models.IntegerField()
+# #   cast_id: Int # PK
+#     book_id = models.CharField(max_length=255) ## link to google-books-api
+#     book_name = models.CharField(max_length=255)
+#     book_image_url = models.CharField(max_length=255)
+#     description = models.TextField()
+
+# class Character(models.Model):
+#     cast_id = models.IntegerField()
+#     character_id = models.IntegerField()
+#     name = models.CharField(max_length=255)
+#     actor = models.CharField(max_length=255)
+#     description = models.TextField()
+#     photo_url = models.CharField(max_length=255)
+    
+# class BookCast_Like(models.Model):
+#     user_id = models.IntegerField()
+#     cast_id = models.IntegerField()
+#     like = models.BooleanField()
+#     comment = models.TextField()
+
+# class Character_Like(models.Model):
+#     user_id = models.IntegerField()
+#     character_id = models.IntegerField()
+#     like = models.BooleanField()
+#     comment = models.TextField()
+    
+    # user {
+    #     username
+    #     email
+    #     bookCasts {
+    #         user_id
+    #         book_id
+    #         likes {
+    #             user_id
+    #             cast_id
+    #             like
+    #             comment
+    #         }
+    #         book_name
+    #         book_image_url
+    #         description
+    #         characters {
+    #             cast_id
+    #             character_id
+    #             name
+    #             actor
+    #             description
+    #             photo_url
+    #             likes {
+    #                 user_id
+    #                 character_id
+    #                 like
+    #                 comment
+    #             }
+    #         }
+            
+    #     }
+    # }
