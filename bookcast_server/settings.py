33f0bd36141cfccfd6deb2234be25de3296f722b
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-03-21 11:42:41 -0400

    update env variable setting

--- bookcast_server/settings.py
+++ bookcast_server/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import dj_database_url 
 import os # add this
 
+os.environ.get('SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
